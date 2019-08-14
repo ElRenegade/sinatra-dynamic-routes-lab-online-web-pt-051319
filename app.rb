71ea443cf8cecc8cba750282b01250d84a81d9dd
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-14 18:51:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,16 +33,16 @@ class App < Sinatra::Base
     @num2 = params[:num2].to_i
     case @operation
       when "add"
-        @num1 + @num2
+        (@num1 + @num2).to_s
 
       when "subtract"
         (@num1 - @num2).to_s
 
       when "divide"
-          @num1 / @num2
+          (@num1 / @num2).to_s
 
       when "multiply"
-        @num1 * @num2
+        (@num1 * @num2).to_s
       end
     end
 end
