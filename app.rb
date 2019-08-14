313d53a2fc8ca4f86b3d34dd1fadb6f384164b32
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-14 17:51:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse!
+    "#{@name}"
+  end
+
+  get '/square/:number'
+  @num = params[:number].to_i
+  @square = @num^2
+  "#{square}"
 
 end
