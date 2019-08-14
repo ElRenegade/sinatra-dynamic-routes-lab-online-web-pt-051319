3203d333db904e6f9a2e550a3d8691fb64f96bce
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-14 18:02:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,4 +11,10 @@ class App < Sinatra::Base
   @num = params[:number].to_i
   "#{@num * @num}"
   end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase} * @number"
+  end
 end
